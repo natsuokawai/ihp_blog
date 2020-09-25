18a72f73f46bc51d578941ab5d45f4167fc6f292
natsuokawai <kawai.natsuo@gmail.com>
2020-09-25 21:58:39 +0900

    Adding Comments

--- Application/Fixtures.sql
+++ Application/Fixtures.sql
@@ -16,9 +16,19 @@ SET SESSION AUTHORIZATION DEFAULT;
 
 ALTER TABLE public.posts DISABLE TRIGGER ALL;
 
-INSERT INTO public.posts (id, title, body, created_at) VALUES ('624ebc2e-c288-447b-b2db-f6f270a54974', 'ddd', 'eee', '2020-09-25 20:38:47.864871+09');
+INSERT INTO public.posts (id, title, body, created_at) VALUES ('624ebc2e-c288-447b-b2db-f6f270a54974', 'ddd', '## eee', '2020-09-25 20:38:47.864871+09');
+INSERT INTO public.posts (id, title, body, created_at) VALUES ('206c18bd-aacc-4b83-8983-20b9253f9636', 'a', '-', '2020-09-25 21:14:14.750503+09');
 
 
 ALTER TABLE public.posts ENABLE TRIGGER ALL;
+
+
+ALTER TABLE public.comments DISABLE TRIGGER ALL;
+
+INSERT INTO public.comments (id, post_id, author, body) VALUES ('e3c11cb1-4101-408b-9f20-023a783e6256', '206c18bd-aacc-4b83-8983-20b9253f9636', 'bob', '');
+INSERT INTO public.comments (id, post_id, author, body) VALUES ('ec823b7d-7951-4d19-b4b4-b59ffc858fff', '206c18bd-aacc-4b83-8983-20b9253f9636', 'aris', 'fda');
+
+
+ALTER TABLE public.comments ENABLE TRIGGER ALL;
 
 
--- Web/View/Posts/Show.hs
+++ Web/View/Posts/Show.hs
@@ -2,7 +2,7 @@ module Web.View.Posts.Show where
 import Web.View.Prelude
 import qualified Text.MMark as MMark
 
-data ShowView = ShowView { post :: Post }
+data ShowView = ShowView { post :: Include "comments" Post }
 
 instance View ShowView ViewContext where
     html ShowView { .. } = [hsx|
@@ -15,9 +15,19 @@ instance View ShowView ViewContext where
         <h1>{get #title post}</h1>
         <p>{get #createdAt post |> timeAgo}</p>
         <div>{get #body post |> renderMarkdown}</div>
+
+        <a href={get #id post |> NewCommentAction}>Add Comment</a>
+        <div>{forEach (get #comments post) renderComment}</div>
     |]
 
 renderMarkdown text = 
     case text |> MMark.parse "" of
         Left error -> "Something went wrong"
         Right markdown -> MMark.render markdown |> tshow |> preEscapedToHtml
+
+renderComment comment = [hsx|
+        <div class="mt-4">
+            <h5>{get #author comment}</h5>
+            <p>{get #body comment}</p>
+        </div>
+    |]
