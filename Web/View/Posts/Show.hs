c08bf7bb99875758081eb4af579e6e563ea63af2
natsuokawai <kawai.natsuo@gmail.com>
2020-09-25 20:40:16 +0900

    Extending the Blog

---
+++ Web/View/Posts/Show.hs
@@ -0,0 +1,23 @@
+module Web.View.Posts.Show where
+import Web.View.Prelude
+import qualified Text.MMark as MMark
+
+data ShowView = ShowView { post :: Post }
+
+instance View ShowView ViewContext where
+    html ShowView { .. } = [hsx|
+        <nav>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href={PostsAction}>Posts</a></li>
+                <li class="breadcrumb-item active">Show Post</li>
+            </ol>
+        </nav>
+        <h1>{get #title post}</h1>
+        <p>{get #createdAt post |> timeAgo}</p>
+        <div>{get #body post |> renderMarkdown}</div>
+    |]
+
+renderMarkdown text = 
+    case text |> MMark.parse "" of
+        Left error -> "Something went wrong"
+        Right markdown -> MMark.render markdown |> tshow |> preEscapedToHtml
